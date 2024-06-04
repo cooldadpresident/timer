8fe40a20f365db9bf0af54bda1ce990721647363
cooldadpresident <bumabuch@gmail.com>
2024-06-04 17:42:15 +0200

    finished

--- timer.py
+++ timer.py
@@ -1,20 +1,26 @@
 import time 
 
-current_time = time.ctime()
-print(current_time)
 
-def countdown_timer(seconds):
-    while seconds > 0:
-        print(f"Time remaining: {seconds} seconds")
+def countdown_timer(total_wait_time):
+    while total_wait_time > 0:
+        print(f"Time remaining: {total_wait_time} seconds", end="\r")
         time.sleep(1)
-        seconds -= 1
-    print('Konec')
+        total_wait_time -= 1
+    if total_wait_time <= 0:
+        print("\n")
+        wait_time_seconds = int(input('Enter seconds '))
+        wait_time_minutes = int(input('Enter minutes '))
+        wait_time_hours = int(input('Enter hours '))
+        total_wait_time = wait_time_hours*3600 + wait_time_minutes*60 + wait_time_seconds
+        
 
+        countdown_timer(total_wait_time)
+        
 # get the number of seconds to wait from the user 
 
 wait_time_seconds = int(input('Enter seconds '))
-wait_time_minutes = int(input('Enter minutes' ))
-wait_time_hours = int(input('Enter hours' ))
+wait_time_minutes = int(input('Enter minutes '))
+wait_time_hours = int(input('Enter hours '))
 
 total_wait_time = wait_time_hours*3600 + wait_time_minutes*60 + wait_time_seconds
 countdown_timer(total_wait_time)
